e25afcb5e9f22560d5e5414c1255a499ac2bbf79
timothygwright <timothy.g.wright@gmail.com>
2016-01-23 15:55:39 -0800

    rails_apps_composer: add README files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123235313) do
+ActiveRecord::Schema.define(version: 20160123235350) do
 
   create_table "payola_affiliates", force: :cascade do |t|
     t.string   "code",       limit: 255
@@ -111,6 +111,15 @@ ActiveRecord::Schema.define(version: 20160123235313) do
 
   add_index "payola_subscriptions", ["guid"], name: "index_payola_subscriptions_on_guid", using: :btree
 
+  create_table "plans", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "stripe_id",  limit: 255
+    t.string   "interval",   limit: 255
+    t.integer  "amount",     limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -124,10 +133,13 @@ ActiveRecord::Schema.define(version: 20160123235313) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.string   "name",                   limit: 255
+    t.integer  "role",                   limit: 4
+    t.integer  "plan_id",                limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["plan_id"], name: "index_users_on_plan_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "users", "plans"
 end
